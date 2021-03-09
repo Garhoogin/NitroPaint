cc25d69ef8f3aed222075991d81e29449ac0a6b1
Garhoogin <declan.c.moore@gmail.com>
2021-03-09 00:43:31 -0600

    Add NSCR paste masking by selection area

--- NitroPaint/nscrviewer.c
+++ NitroPaint/nscrviewer.c
@@ -445,11 +445,27 @@ LRESULT WINAPI NscrViewerWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPar
 							int tilesX = (clip[5] & 0xF) | ((clip[6] & 0xF) << 4);
 							int tilesY = (clip[7] & 0xF) | ((clip[8] & 0xF) << 4);
 
+							int minX = 0, minY = 0, maxX = data->nscr.nWidth / 8, maxY = data->nscr.nHeight / 8;
+							if (data->selStartX != -1 && data->selStartY != -1) {
+								int sminX = min(data->selStartX, data->selEndX);
+								int smaxX = max(data->selStartX, data->selEndX);
+								int sminY = min(data->selStartY, data->selEndY);
+								int smaxY = max(data->selStartY, data->selEndY);
+								if (tileX >= sminX && tileX <= smaxX && tileY >= sminY && tileY <= smaxY) {
+									minX = sminX;
+									minY = sminY;
+									maxX = smaxX;
+									maxY = smaxY;
+								}
+							}
+
 							int i = 0;
 							for (int y = tileY; y < tileY + tilesY; y++) {
 								for (int x = tileX; x < tileX + tilesX; x++) {
 									WORD d = (clip[9 + i * 4] & 0xF) | ((clip[9 + i * 4 + 1] & 0xF) << 4) | ((clip[9 + i * 4 + 2] & 0xF) << 8) | ((clip[9 + i * 4 + 3] & 0xF) << 12);
-									if (x < data->nscr.nWidth / 8 && y < data->nscr.nHeight / 8) data->nscr.data[x + y * (data->nscr.nHeight / 8)] = d;
+									if (x < data->nscr.nWidth / 8 && y < data->nscr.nHeight / 8) {
+										if(x >= minX && x <= maxX && y >= minY && y <= maxY) data->nscr.data[x + y * (data->nscr.nHeight / 8)] = d;
+									}
 
 									i++;
 								}
