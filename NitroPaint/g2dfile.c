2746f00e759d7f3f6c890516763e7a880b1801a7
Garhoogin <declan.c.moore@gmail.com>
2022-07-28 03:04:04 -0500

    Use stdint types in g2dfile

--- NitroPaint/g2dfile.c
+++ NitroPaint/g2dfile.c
@@ -1,24 +1,24 @@
 #include <stdio.h>
-
+#include <stdint.h>
 
 int g2dIsValid(char *buffer, unsigned int size) {
 	if (size < 0x10) return 0;
-	unsigned short endianness = *(unsigned short *) (buffer + 4);
+	uint16_t endianness = *(uint16_t *) (buffer + 4);
 	if (endianness != 0xFFFE && endianness != 0xFEFF) return 0;
-	int fileSize = *(int *) (buffer + 8);
+	uint32_t fileSize = *(uint32_t *) (buffer + 8);
 	if (fileSize != size) {
 		fileSize = (fileSize + 3) & ~3; 
 		if (fileSize != size) return 0;
 	}
-	unsigned short headerSize = *(unsigned short *) (buffer + 0xC);
+	uint16_t headerSize = *(uint16_t *) (buffer + 0xC);
 	if (headerSize < 0x10) return 0;
-	int nSections = *(unsigned short *) (buffer + 0xE);
+	int nSections = *(uint16_t *) (buffer + 0xE);
 	
 	//check sections
 	unsigned int offset = headerSize;
 	for (int i = 0; i < nSections; i++) {
 		if (offset + 8 > size) return 0;
-		unsigned int size = *(unsigned int *) (buffer + offset + 4);
+		uint32_t size = *(uint32_t *) (buffer + offset + 4);
 		offset += size;
 	}
 
@@ -26,16 +26,16 @@ int g2dIsValid(char *buffer, unsigned int size) {
 }
 
 int g2dGetNumberOfSections(char *buffer, unsigned int size) {
-	return *(unsigned short *) (buffer + 0xE);
+	return *(uint16_t *) (buffer + 0xE);
 }
 
 char *g2dGetSectionByIndex(char *buffer, unsigned int size, int index) {
 	if (index >= g2dGetNumberOfSections(buffer, size)) return NULL;
 
-	unsigned int offset = *(unsigned short *) (buffer + 0xC);
+	uint32_t offset = *(uint16_t *) (buffer + 0xC);
 	for (int i = 0; i <= index; i++) {
 		if (offset + 8 > size) return NULL;
-		unsigned int size = *(unsigned int *) (buffer + offset + 4);
+		uint32_t size = *(uint32_t *) (buffer + offset + 4);
 
 		if (i == index) return buffer + offset;
 		offset += size;
@@ -45,12 +45,12 @@ char *g2dGetSectionByIndex(char *buffer, unsigned int size, int index) {
 
 char *g2dGetSectionByMagic(char *buffer, unsigned int size, unsigned int sectionMagic) {
 	int nSections = g2dGetNumberOfSections(buffer, size);
-	unsigned int offset = *(unsigned short *) (buffer + 0xC);
+	uint32_t offset = *(uint16_t *) (buffer + 0xC);
 
 	for (int i = 0; i <= nSections; i++) {
 		if (offset + 8 > size) return NULL;
-		unsigned int magic = *(unsigned int *) (buffer + offset);
-		unsigned int size = *(unsigned int *) (buffer + offset + 4);
+		uint32_t magic = *(uint32_t *) (buffer + offset);
+		uint32_t size = *(uint32_t *) (buffer + offset + 4);
 
 		if (magic == sectionMagic) return buffer + offset;
 		offset += size;
