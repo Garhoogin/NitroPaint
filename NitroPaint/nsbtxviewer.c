82b724d64926341c898f6ed7464ee56499bf71b9
Garhoogin <declan.c.moore@gmail.com>
2023-03-26 20:37:29 -0500

    Direct texture replacement doesn't touch palette

--- NitroPaint/nsbtxviewer.c
+++ NitroPaint/nsbtxviewer.c
@@ -468,10 +468,10 @@ LRESULT WINAPI NsbtxViewerWndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lPa
 							PALETTE *destPal = data->nsbtx.palettes + selectedPalette;
 							int oldTexImageParam = destTex->texImageParam;
 							memcpy(texels.name, destTex->name, 16);
-							memcpy(palette.name, destPal->name, 16);
 							if (destTex->cmp) free(destTex->cmp);
 							memcpy(destTex, &texels, sizeof(TEXELS));
 							if (FORMAT(texels.texImageParam) != CT_DIRECT) {
+								memcpy(palette.name, destPal->name, 16);
 								free(destPal->pal);
 								memcpy(destPal, &palette, sizeof(PALETTE));
 							}
