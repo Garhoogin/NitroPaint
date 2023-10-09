b9fa4bc8e857e78a70367374a3b744941fd81954
Garhoogin <declan.c.moore@gmail.com>
2023-10-09 12:33:52 -0500

    Delete NitroPaint/analysis.h

--- NitroPaint/analysis.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <Windows.h>
-
-DWORD getAverageColor(DWORD *colors, int nColors);
-
-void getPrincipalComponent(DWORD *colors, int nColors, float *vec);
-
-void getColorEndPoints(DWORD *colors, int nColors, DWORD *points);
