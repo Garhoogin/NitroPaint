3bded6b8d298e27dc668ff36443f0ce6166fbd6b
Garhoogin <declan.c.moore@gmail.com>
2026-06-06 01:29:19 -0500

    Clean up color API

--- NitroPaint/color.c
+++ NitroPaint/color.c
@@ -1,11 +1,35 @@
+// -----------------------------------------------------------------------------------------------
+// Copyright (c) 2020, Garhoogin
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// -----------------------------------------------------------------------------------------------
 #include "color.h"
 
-static uint8_t colorConversionLookupReverse[] = {
-	0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,
-	2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,
-	4,  4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  5,  5,  5,  6,  6,
-	6,  6,  6,  6,  6,  6,  7,  7,  7,  7,  7,  7,  7,  7,  8,  8,
-	8,  8,  8,  8,  8,  8,  9,  9,  9,  9,  9,  9,  9,  9,  9,  10,
+static const uint8_t sLookup_8_to_5[] = {
+	 0,  0,  0,  0,  0,  1,  1,  1,  1,  1,  1,  1,  1,  2,  2,  2,
+	 2,  2,  2,  2,  2,  3,  3,  3,  3,  3,  3,  3,  3,  4,  4,  4,
+	 4,  4,  4,  4,  4,  4,  5,  5,  5,  5,  5,  5,  5,  5,  6,  6,
+	 6,  6,  6,  6,  6,  6,  7,  7,  7,  7,  7,  7,  7,  7,  8,  8,
+	 8,  8,  8,  8,  8,  8,  9,  9,  9,  9,  9,  9,  9,  9,  9, 10,
 	10, 10, 10, 10, 10, 10, 10, 11, 11, 11, 11, 11, 11, 11, 11, 12,
 	12, 12, 12, 12, 12, 12, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13,
 	14, 14, 14, 14, 14, 14, 14, 14, 15, 15, 15, 15, 15, 15, 15, 15,
@@ -19,28 +43,19 @@ static uint8_t colorConversionLookupReverse[] = {
 	29, 29, 29, 30, 30, 30, 30, 30, 30, 30, 30, 31, 31, 31, 31, 31
 };
 
-COLOR ColorConvertToDS(COLOR32 c) {
-	int r = colorConversionLookupReverse[c & 0xFF];
-	int g = colorConversionLookupReverse[(c >> 8) & 0xFF];
-	int b = colorConversionLookupReverse[(c >> 16) & 0xFF];
-	return r | (g << 5) | (b << 10);
-}
-
-static uint8_t colorConversionLookup[] = {
-	0,   8,   16,  25,  33,  41,  49,  58,
-	66,  74,  82,  90,  99,  107, 115, 123,
-	132, 140, 148, 156, 165, 173, 181, 189,
-	197, 206, 214, 222, 230, 239, 247, 255
+static const uint8_t sLookup_5_to_8[] = {
+	  0,   8,  16,  25,  33,  41,  49,  58,  66,  74,  82,  90,  99, 107, 115, 123,
+	132, 140, 148, 156, 165, 173, 181, 189, 197, 206, 214, 222, 230, 239, 247, 255
 };
 
-static uint8_t colorRound6Lookup[] = {
-	0,   0,   0,   4,   4,   4,   4,   8,   8,   8,   8,   12,  12,  12,  12,  16,
-	16,  16,  16,  20,  20,  20,  20,  24,  24,  24,  24,  28,  28,  28,  28,  32,
-	32,  32,  32,  36,  36,  36,  36,  40,  40,  40,  40,  45,  45,  45,  45,  49,
-	49,  49,  49,  53,  53,  53,  53,  57,  57,  57,  57,  61,  61,  61,  61,  65,
-	65,  65,  65,  69,  69,  69,  69,  73,  73,  73,  73,  77,  77,  77,  77,  81,
-	81,  81,  81,  85,  85,  85,  85,  85,  89,  89,  89,  89,  93,  93,  93,  93,
-	97,  97,  97,  97,  101, 101, 101, 101, 105, 105, 105, 105, 109, 109, 109, 109,
+static const uint8_t sLookup_8_to_6_to_8[] = {
+	  0,   0,   0,   4,   4,   4,   4,   8,   8,   8,   8,  12,  12,  12,  12,  16,
+	 16,  16,  16,  20,  20,  20,  20,  24,  24,  24,  24,  28,  28,  28,  28,  32,
+	 32,  32,  32,  36,  36,  36,  36,  40,  40,  40,  40,  45,  45,  45,  45,  49,
+	 49,  49,  49,  53,  53,  53,  53,  57,  57,  57,  57,  61,  61,  61,  61,  65,
+	 65,  65,  65,  69,  69,  69,  69,  73,  73,  73,  73,  77,  77,  77,  77,  81,
+	 81,  81,  81,  85,  85,  85,  85,  85,  89,  89,  89,  89,  93,  93,  93,  93,
+	 97,  97,  97,  97, 101, 101, 101, 101, 105, 105, 105, 105, 109, 109, 109, 109,
 	113, 113, 113, 113, 117, 117, 117, 117, 121, 121, 121, 121, 125, 125, 125, 125,
 	130, 130, 130, 130, 134, 134, 134, 134, 138, 138, 138, 138, 142, 142, 142, 142,
 	146, 146, 146, 146, 150, 150, 150, 150, 154, 154, 154, 154, 158, 158, 158, 158,
@@ -52,30 +67,31 @@ static uint8_t colorRound6Lookup[] = {
 	239, 243, 243, 243, 243, 247, 247, 247, 247, 251, 251, 251, 251, 255, 255, 255
 };
 
+COLOR ColorConvertToDS(COLOR32 c) {
+	unsigned int r = sLookup_8_to_5[(c >>  0) & 0xFF];
+	unsigned int g = sLookup_8_to_5[(c >>  8) & 0xFF];
+	unsigned int b = sLookup_8_to_5[(c >> 16) & 0xFF];
+	return r | (g << 5) | (b << 10);
+}
+
 COLOR32 ColorConvertFromDS(COLOR c) {
-	int r = colorConversionLookup[GetR(c)];
-	int g = colorConversionLookup[GetG(c)];
-	int b = colorConversionLookup[GetB(c)];
+	unsigned int r = sLookup_5_to_8[GetR(c)];
+	unsigned int g = sLookup_5_to_8[GetG(c)];
+	unsigned int b = sLookup_5_to_8[GetB(c)];
 	return r | (g << 8) | (b << 16);
 }
 
 COLOR32 ColorRoundToDS15(COLOR32 c) {
-	int r = (c >> 0) & 0xFF;
-	int g = (c >> 8) & 0xFF;
-	int b = (c >> 16) & 0xFF;
-	r = colorConversionLookup[colorConversionLookupReverse[r]];
-	g = colorConversionLookup[colorConversionLookupReverse[g]];
-	b = colorConversionLookup[colorConversionLookupReverse[b]];
+	unsigned int r = sLookup_5_to_8[sLookup_8_to_5[(c >>  0) & 0xFF]];
+	unsigned int g = sLookup_5_to_8[sLookup_8_to_5[(c >>  8) & 0xFF]];
+	unsigned int b = sLookup_5_to_8[sLookup_8_to_5[(c >> 16) & 0xFF]];
 	return r | (g << 8) | (b << 16);
 }
 
 COLOR32 ColorRoundToDS18(COLOR32 c) {
-	int r = (c >> 0) & 0xFF;
-	int g = (c >> 8) & 0xFF;
-	int b = (c >> 16) & 0xFF;
-	r = colorRound6Lookup[r];
-	g = colorRound6Lookup[g];
-	b = colorRound6Lookup[b];
+	unsigned r = sLookup_8_to_6_to_8[(c >>  0) & 0xFF];
+	unsigned g = sLookup_8_to_6_to_8[(c >>  8) & 0xFF];
+	unsigned b = sLookup_8_to_6_to_8[(c >> 16) & 0xFF];
 	return r | (g << 8) | (b << 16);
 }
 
--- NitroPaint/color.h
+++ NitroPaint/color.h
@@ -1,48 +1,107 @@
+// -----------------------------------------------------------------------------------------------
+// Copyright (c) 2020, Garhoogin
+// All rights reserved.
+// 
+// Redistribution and use in source and binary forms, with or without modification, are permitted
+// provided that the following conditions are met:
+// 
+// 1. Redistributions of source code must retain the above copyright notice, this list of
+//    conditions and the following disclaimer.
+// 
+// 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+//    conditions and the following disclaimer in the documentation and/or other materials provided
+//    with the distribution.
+// 
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+// IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY
+// AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+// CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+// THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+// OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+// -----------------------------------------------------------------------------------------------
 #pragma once
 
 #include <stdint.h>
 
-typedef uint16_t COLOR;
-typedef uint32_t COLOR32;
+typedef uint16_t COLOR;    // 15-bit color. BGR555.
+typedef uint32_t COLOR32;  // 32-bit color. ABGR8888
 
 #define COLOR_CHANNEL_R 0
 #define COLOR_CHANNEL_G 1
 #define COLOR_CHANNEL_B 2
 
-#define GetR(c) ((c)&0x1F)
-#define GetG(c) (((c)>>5)&0x1F)
+#define GetR(c) (((c)>> 0)&0x1F)
+#define GetG(c) (((c)>> 5)&0x1F)
 #define GetB(c) (((c)>>10)&0x1F)
-#define GetA(c) (((c)>>15)&1)
+#define GetA(c) (((c)>>15)&0x01)
 
 #define REVERSE(x) ((x)&0xFF00FF00)|(((x)&0xFF)<<16)|(((x)>>16)&0xFF)
 
 #define CREVERSE(x) (((x)&0x83E0)|(((x)&0x1F)<<10)|(((x)>>10)&0x1F))
 
-#define ColorSetChannel(c,ch,val) ((c)=(COLOR)(((c)&((0x1F<<((ch)*5))^0x7FFF))|(val)<<((ch)*5)))
-
-#define ColorCreate(r,g,b) ((COLOR)((r)|((g)<<5)|((b)<<10)))
-
+// -----------------------------------------------------------------------------------------------
+// Name: ColorConvertToDS
 //
-// Convert 24-bit RGB color to 15-bit color
+// Converts a 24-bit RGB color to a 15-bit RGB color by scaling each color channel independently
+// from a scale of 255 to a scale of 31, rounding to the nearest whole number. The returned color
+// has the most significant bit clear.
 //
+// Parameters:
+//   c             The 24-bit color to convert.
+//
+// Returns:
+//   The converted color.
+// -----------------------------------------------------------------------------------------------
 COLOR ColorConvertToDS(COLOR32 c);
 
+// -----------------------------------------------------------------------------------------------
+// Name: ColorConvertFromDS
 //
-// Convert 15-bit RGB color to 24-bit color
+// Converts a 15-bit RGB color to a 24-bit RGB color by scaling each color channel independently
+// from a scale of 31 to a scale of 255, rounding to the nearest whole number. The returned color
+// has the most significant 8 bits clear.
 //
+// Parameters:
+//   c             The 24-bit color to convert.
+//
+// Returns:
+//   The converted color.
+// -----------------------------------------------------------------------------------------------
 COLOR32 ColorConvertFromDS(COLOR c);
 
+// -----------------------------------------------------------------------------------------------
+// Name: ColorRoundToDS15
 //
-// Round a 24-bit RGB color to the nearest 15-bit representable 24-bit color
+// Converts a 24-bit color to its nearest 15-bit color and then back, as though by calling the
+// ColorConvertToDS and ColorConvertFromDS routines. The returned color has its most significant
+// bit clear.
 //
+// Parameters:
+//   c             The 24-bit color to convert.
+//
+// Returns:
+//   The converted color.
+// -----------------------------------------------------------------------------------------------
 COLOR32 ColorRoundToDS15(COLOR32 c);
 
+// -----------------------------------------------------------------------------------------------
+// Name: ColorRoundToDS18
 //
-// Round a 24-bit RGB color to the nearest 18-bit representable 24-bit color
+// Converts a 24-bit color to its nearest 18-bit color and then back.
 //
+// Parameters:
+//   c             The 24-bit color to convert.
+//
+// Returns:
+//   The converted color.
+// -----------------------------------------------------------------------------------------------
 COLOR32 ColorRoundToDS18(COLOR32 c);
 
 //
 // Interpolate between two 15-bit colors
+// Deprecated do not use
 //
 COLOR ColorInterpolate(COLOR c1, COLOR c2, float amt);
